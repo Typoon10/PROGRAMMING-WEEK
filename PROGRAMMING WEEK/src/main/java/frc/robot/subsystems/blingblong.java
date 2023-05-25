5b18c7b5ef71b8ad49d71476f2027c54fb206bc3
Titans2620 <starobotics2620@gmail.com>
2023-05-25 16:49:42 -0400

    Stuff

--- PROGRAMMING WEEK/src/main/java/frc/robot/subsystems/blingblong.java
+++ PROGRAMMING WEEK/src/main/java/frc/robot/subsystems/blingblong.java
@@ -4,17 +4,48 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+
 public class blingblong extends SubsystemBase {
+
+
   /** Creates a new blingblong. */
-  private int leng;
-  public blingblong(int leng) {
-  
+  private AddressableLED blinkblank = new AddressableLED(9);
+  private AddressableLEDBuffer blankblink = new AddressableLEDBuffer(5);
+  private int rainbow;
+  private void rgb() {
+    // For every pixel
+    for (var i = 0; i < blankblink.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = (rainbow + (i * 180 / blankblink.getLength())) % 180;
+      // Set the value
+      blankblink.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    rainbow += 3;
+    // Check bounds
+    rainbow %= 180;
   }
+  public blingblong() {
+      rgb();
+      
+      blinkblank.setLength(blankblink.getLength());
+      blinkblank.setData(blankblink);
+    
+    
+    
+    }
+    
+  
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    blinkblank.start();
   }
 }
